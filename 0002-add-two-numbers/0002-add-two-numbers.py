f73faf913b3e0cbb55d058bcd3fbca743149423d
Chaitanya Jindal <jindalchaitanya@icloud.com>
2024-12-21 00:18:16 +0530

    Time: 10 ms (11.5%), Space: 17.8 MB (7.63%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,24 +1,19 @@
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        dummyHead = ListNode(0)
-        tail = dummyHead
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        out = ListNode()
+        tail = out
         carry = 0
-
-        while l1 is not None or l2 is not None or carry != 0:
-            digit1 = l1.val if l1 is not None else 0
-            digit2 = l2.val if l2 is not None else 0
-
-            sum = digit1 + digit2 + carry
-            digit = sum % 10
-            carry = sum // 10
-
-            newNode = ListNode(digit)
-            tail.next = newNode
+        while l1 or l2 or carry:
+            first = l1.val if l1 else 0
+            second = l2.val if l2 else 0
+            summ = first + second + carry
+            if summ > 9:
+                carry = int(str(summ)[0])
+                tail.next = ListNode(int(str(summ)[1]))
+            else:
+                carry = 0
+                tail.next = ListNode(summ)
             tail = tail.next
-
-            l1 = l1.next if l1 is not None else None
-            l2 = l2.next if l2 is not None else None
-
-        result = dummyHead.next
-        dummyHead.next = None
-        return result
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return out.next
